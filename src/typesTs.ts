48b6cf006628d2a43435841ea088bf93d5e1d848
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-12-09 16:22:33 +0530

    types

---
+++ src/typesTs.ts
@@ -0,0 +1,4 @@
+let cups = Math.random()>0.5?10:'5'; //type inferencing ts compiler detects the type automatically
+
+let chai:string ="Masala chai" //type annotation means we need to define the type manually
+
